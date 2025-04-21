9739907cabb8f8369cc12543ae8fb943872682d3
masdonte <poonic54@gmail.com>
2025-04-22 00:32:10 +0200

    fix monitors to monitors + put hyprpaper as services

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -3,23 +3,40 @@
 {
 
 
+	services.hyprpaper = {
+		enable = true;
+	};
 	wayland.windowManager.hyprland = {
 		enable = true;
 
 		settings = {
 			"$mod1" = "SUPER";
+			"$mod2" = "SUPERSHIFT";
 			"$terminal" = "foot";
 			"$browser" = "firefox";
 			"$editor" = "neovim";
 
-			monitors = [
-
-				"eDP-1,1920x1080@60,0x0, 1"
+			monitor = [
+				"eDP-1, 1920x1080@60, 0x0, 1"
 
 			];
+			env = [
+				"QT_QPA_PLATFORM,wayland"
+					"SDL_VIDEODRIVER,wayland"
+					"CLUTTER_BACKEND,wayland"
+					"GDK_BACKEND,wayland"
+					"MOZ_ENABLE_WAYLAND,1"
+					"MOZ_WEBRENDER,1"
+					"OZONE_PLATFORM,wayland"
+					"NIXOS_OZONE_WL,1"
+					"SDL_VIDEODRIVER,wayland"
+
+];
 
 			exec-once = [
-				"foot"
+				"foot &"
+				"nm-applet &"
+				"firefox &"
 			];
 
 			general = {
@@ -29,17 +46,21 @@
 			animations = {
 				enabled = true;
 
+
 			};
 
 
 			input = {
 				kb_layout = "fr";
+				kb_options = "caps:swapescape";
 
 			};
 
 			bind = [
 				"$mod1, Q, killactive"
 					"$mod1, Return, exec, $terminal"
+					"$mod2, Q, exec, uwsm stop"
+
 			];
 
 		};
