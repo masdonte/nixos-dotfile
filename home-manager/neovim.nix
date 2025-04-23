a7258a82140861fd217a542fb014c5e4e0548b76
masdonte <poonic54@gmail.com>
2025-04-23 19:56:57 +0200

    neovim basic configuration

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,6 +1,12 @@
-{config, pkgs, ...}{
+{config, pkgs, ...}:{
 
 	programs.neovim = {
 		enable = true;
+		viAlias = true;
+		vimAlias = true;
+
+		plugins = with pkgs.vimPlugins; [
+# Language servers
+			vim-nix];
 	};
-		   }
+		    }
