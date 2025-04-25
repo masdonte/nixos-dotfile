d0c3a04495f56c6b6d8aacc6501157e1c0392394
masdonte <poonic54@gmail.com>
2025-04-25 12:50:29 +0200

    add tlp for laptop

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,7 @@
 		enable = true;
 		pulse.enable = true;
 	};
-
+ 
   users = {
     defaultUserShell = "/etc/profiles/per-user/reda/bin/zsh";
     users.reda = {
@@ -63,6 +63,28 @@
       extraGroups = [ "wheel" "users" "audio" "video" "adbusers" ];
     };
   };
+
+
+
+  services.tlp = {
+  enable = true;
+  settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+    CPU_MIN_PERF_ON_AC = 0;
+    CPU_MAX_PERF_ON_AC = 100;
+    CPU_MIN_PERF_ON_BAT = 0;
+    CPU_MAX_PERF_ON_BAT = 20;
+
+    # Optional helps save long term battery health
+    START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+    STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+  };
+};
 
 security.doas.enable = true;
 security.sudo.enable = true;
