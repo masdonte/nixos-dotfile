11f10d634d526752ef3e3d4ef53445c3ee2c5371
masdonte <poonic54@gmail.com>
2025-04-28 10:19:44 +0200

    add configuration for foot.nix

--- home-manager/foot.nix
+++ home-manager/foot.nix
@@ -1,5 +1,26 @@
 { config, pkgs, ...}:
 {
 
-programs.foot.enable = true;
+  programs.foot = { 
+    enable = true;
+    settings = {
+      main = 
+      {
+        font = "monospace:size=10";
+      };
+      cursor = {
+        style = "beam";
+        beam-thickness = 1;
+
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+      colors = {
+        alpha = "0.9";
+        flash = "7f7f00";
+        flash-alpha = "0.5";
+      };
+    };
+  };
 }
