c5665e00af263adaa321f9becfd11c9608a146e8
masdonte <poonic54@gmail.com>
2025-08-11 18:23:46 +0200

    basic config for mpv

--- home-manager/mpv.nix
+++ home-manager/mpv.nix
@@ -1,8 +1,26 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
-
   programs.mpv = {
     enable = true;
-  };
-}
+
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          sponsorblock
+        ];
+
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+        };
+      }
+      );
+
+      config = {
+        profile = "fast";
+        hwdec = "auto";
+        ytdl-format = "bestvideo[heigh<=?1080]+bestaudio";
+      };
+    };
+  }
 
