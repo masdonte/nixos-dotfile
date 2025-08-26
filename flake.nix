0ce7bd66554c81ecf2d65b28bdc21b8672d1f092
masdonte <poonic54@gmail.com>
2025-08-26 18:27:18 +0200

    add nvf

--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix";
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, catppuccin, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, catppuccin, home-manager, nvf, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -20,6 +24,7 @@
             home-manager.useUserPackages = true;
             home-manager.users.reda = {
               imports = [
+                inputs.nvf.homeManagerModules.default
                 ./home-manager/home.nix
                 catppuccin.homeModules.catppuccin
               ];
