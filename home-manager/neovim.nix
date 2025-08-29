f55216c6ef7bdd99d424ffe8426dc0d3addd96c6
masdonte <poonic54@gmail.com>
2025-08-29 15:39:25 +0200

    add zed editr w/basic config

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,24 +1,40 @@
 {config, pkgs, ...}:{
 
-  programs.nvf = {
-    enable = true;
-    settings = {
-      vim = {
-        viAlias = false;
-        vimAlias = true;
-        lsp.enable = true;
+        programs.nvf = {
+                enable = true;
+                settings = {
+                        vim = {
+                                viAlias = false;
+                                vimAlias = true;
+                                lsp.enable = true;
 
-        telescope.enable = true;
-        statusline.lualine.enable = true;
+                                telescope.enable = true;
+                                statusline.lualine.enable = true;
 
-        languages = {
-          nix.enable = true;
+                                languages = {
+                                        nix.enable = true;
+                                };
+                        };
+
+
+                };
         };
-      };
+        programs.zed-editor = {
+                enable = true;
+                extensions =  [ "nix"];
+                userSettings = {
+                        features = {
+                                copilot = false;
+                        };
+                        telemetry = {
+                                metrics = false;
+                                diagnostics = false:
 
-
-    };
-  }
-  ;
-}
+                        };
+                        vim_mode = true;
+                        ui_font_size = 16;
+                        buffer_font_size = 16;
+                };
+        };
+                    }
 
